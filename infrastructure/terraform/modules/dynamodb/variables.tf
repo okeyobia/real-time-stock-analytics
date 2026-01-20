e244f4fcff656109d2abe00aeb04fe48f1c70d01
Okey <okey@Okeys-MacBook-Air.local>
2026-01-20 12:02:41 -0600

    add variables dynamodb/main

--- infrastructure/terraform/modules/dynamodb/variables.tf
+++ infrastructure/terraform/modules/dynamodb/variables.tf
@@ -11,3 +11,26 @@ variable "tags" {
   type    = map(string)
   default = {}
 }
+
+variable "function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
+
+variable "lambda_zip" {
+  description = "Path to the Lambda deployment package"
+  type        = string
+}
+
+variable "dynamodb_table" {
+  description = "Name of the DynamoDB table"
+  type        = string
+}
+
+variable "s3_bucket" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+// ...existing code...
+
